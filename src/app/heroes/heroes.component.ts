b8cd36de377270906f148f54778167e1a74ad9a3
Alex Lepore <alexlepore990@gmail.com>
2023-06-14 09:55:10 -0400

    app displays list of heroes, user can select hero and see details, *ngfor display list, *ngif to conditionally include or exclude html, you can toggle a css style class wiht a class binding.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,16 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+  heroes = HEROES;
+  //hero = property, Hero = type of property(initialized with id of 1...etc)
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  //safeloading property selected here using ?
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
