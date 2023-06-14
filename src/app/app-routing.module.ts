fd1e182e961925d47d3cceb916ed13dddff46fb8
Alex Lepore <alexlepore990@gmail.com>
2023-06-14 12:32:07 -0400

    added ang router to nav components, turned app comp into nav shell with <a> links and <router-outlet>, configed the router in an approutingmodule, defined routes-redirected a route-paramatized a route, used routerlink directive in anchor elements, refractedf a tightly coupled main/detail view into a routed detail view, used router link params to nav to the detail view of user-selected hero, shared the heroservice with other comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+//HeroesComponent, gives the Router somewhere to go once you configure the routes.
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+
+//Routes tell the Router which view to display when a user clicks a
+// link or pastes a URL into the browser address bar.
+const routes: Routes = [
+  //path	A string that matches the URL in the browser address bar.
+  //The component that the router should create when navigating to this route.
+  // ex: localhost:4200/heroes.
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  //The colon : character in the path indicates that :id is a placeholder for a specific hero id.
+];
 
 @NgModule({
+  //The following line adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step by calling RouterModule.forRoot():
   imports: [RouterModule.forRoot(routes)],
+  //The method is called forRoot() because you configure the router at the application's root level. The forRoot() method supplies the service providers 
+  //and directives needed for routing, and performs the initial navigation based on the current browser URL.
   exports: [RouterModule]
+  //Next, AppRoutingModule exports RouterModule to be available throughout the application.
 })
 export class AppRoutingModule { }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,7 +14,25 @@ export class HeroDetailComponent {
   //The hero property must be an Input property, 
   //annotated with the @Input() decorator, because the external HeroesComponent binds to it like this.
   //<app-hero-detail [hero]="selectedHero"></app-hero-detail>
-  @Input() hero?: Hero;
-}
+  hero: Hero | undefined;
 
-//
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
